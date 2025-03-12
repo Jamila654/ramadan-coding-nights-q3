ed7d57ab609f6264d0e99a363ded6541ec764cda
Jamila654 <136564835+Jamila654@users.noreply.github.com>
2025-03-12 22:16:44 +0500

    updated

--- deployment/main.py
+++ deployment/main.py
@@ -1,10 +1,14 @@
 #type: ignore
 import streamlit as st
 
-
+st.set_page_config(page_title="Simple Streamlit App", page_icon=":smiley:")
 st.title("Simple Streamlit App")
  
 user_input = st.text_input("Enter some text:")
 
 if st.button("Show Text"):
      st.write(f"You entered: {user_input}") 
+     
+
+st.write("---")
+st.write("Made with :heart: by Jam")
